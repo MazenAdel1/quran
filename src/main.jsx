6e4054971c48bae1e9591a3a087b1fa153f7db4a
MazenAdel1 <ma134652134652@gmail.com>
2023-11-24 02:42:11 +0200

    try to fix routing issues

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/quran">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
