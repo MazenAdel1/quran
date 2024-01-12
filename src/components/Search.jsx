adeb8cccdf2b2bcac34733f2df9620ef6fac0917
Mazen Adel <ma134652134652@gmail.com>
2024-01-13 00:19:23 +0200

    commenting the file

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,76 +1,130 @@
+// //
+// //
+// // future Idea
+// //
+// //
+
 // import { useEffect, useRef, useState } from "react";
 // import { Link } from "react-router-dom";
 
 // export default function Search() {
-//   const [search, setSearch] = useState("");
-//   const [searchResult, setSearchResult] = useState({
-//     results: [],
-//     totalResults: 0,
-//   });
+//   // https://api.quran.com/api/v4/search?q=${search}&size=50&page=${currentApiResultPage}
+
+//   const [results, setResults] = useState([]);
+//   const [resultApiPages, setResultApiPages] = useState(0);
 //   const [currentApiResultPage, setCurrentApiResultPage] = useState(1);
-//   const [totalApiResultsPages, setTotalApiResultsPages] = useState(1);
+//   const searchResultsRef = useRef();
+//   const inputRef = useRef();
 
-//   const searchResultRef = useRef();
+//   const fetchData = async (input) => {
+//     const response = await fetch(
+//       `https://api.quran.com/api/v4/search?q=${input.trim()}&size=50&page=${currentApiResultPage}`,
+//     );
+//     const data = await response.json();
 
-//   const handleChange = (e) => {
-//     if (e.target.value === "") {
-//       searchResultRef.current.classList.add(`hidden`);
-//       document.body.classList.remove(`overflow-hidden`);
-//     } else {
-//       searchResultRef.current.classList.remove(`hidden`);
-//       document.body.classList.add(`overflow-hidden`);
+//     setResultApiPages(data.search.total_pages);
+
+//     if (data.search.total_results) {
+//       const fullData = data.search.results.map(async (e) => {
+//         const res = await fetch(
+//           `https://api.quran.com/api/v4/verses/by_key/${e.verse_key}`,
+//         );
+//         const pageNumber = await res.json();
+
+//         return { ayah: e.text, pageNumber: pageNumber.verse.page_number };
+//       });
+//       Promise.all(fullData).then((D) => {
+//         const sortedData = D.sort((a, b) => a.pageNumber - b.pageNumber);
+//         setResults(sortedData);
+//       });
 //     }
-//     setSearch(e.target.value);
+//   };
+
+//   const handleChange = async (input) => {
+//     document.body.classList.add(`overflow-y-hidden`);
+//     searchResultsRef.current.classList.remove(`hidden`);
+//     if (input.trim() === ``) {
+//       handleClose();
+//     }
+
 //     setCurrentApiResultPage(1);
+
+//     fetchData(input);
+//   };
+
+//   const handleClose = () => {
+//     searchResultsRef.current.classList.add(`hidden`);
+//     document.body.classList.remove(`overflow-y-hidden`);
 //   };
 
 //   useEffect(() => {
-//     fetch(
-//       `https://api.quran.com/api/v4/search?q=${search}&page=${currentApiResultPage}`
-//     )
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setSearchResult({
-//           results: data.search.results,
-//           totalResults: data.search.total_results,
-//         });
-//         setTotalApiResultsPages(data.search.total_pages);
-//       });
-//   }, [search, currentApiResultPage]);
-
-//   const handleClick = () => {
-//     setCurrentApiResultPage(currentApiResultPage + 1);
-//   };
+//     if (inputRef.current.value) {
+//       fetchData(inputRef.current.value);
+//     }
+//   }, [currentApiResultPage]);
 
 //   return (
 //     <>
-//       <div className="w-1/2 mx-auto py-2">
-//         <input
-//           type="text"
-//           placeholder="البحث عن آية..."
-//           className="w-full border-none outline-none px-2 py-1 rounded-md bg-primary-white"
-//           onChange={handleChange}
-//         />
-//       </div>
+//       <input
+//         type="text"
+//         placeholder="البحث عن آية..."
+//         ref={inputRef}
+//         className="mx-auto my-2 block w-[calc(100%-160px)] rounded-md px-2 py-1 outline-none sm:w-1/2"
+//         onChange={(e) => handleChange(e.target.value)}
+//       />
 
 //       <div
-//         className="w-full h-[calc(100dvh-60px)] bg-[#000000ce] absolute z-50 bottom-0 px-4 py-4 hidden overflow-auto"
-//         ref={searchResultRef}
+//         ref={searchResultsRef}
+//         className="absolute z-50 hidden h-[calc(100%-52px)] w-full overflow-y-auto bg-[#000000] py-2"
 //       >
-//         <span className="text-white block text-lg pb-6">
-//           مجموع النتائج: {searchResult.totalResults}
-//         </span>
-
-//         <div className="flex flex-col gap-7 text-2xl">
-//           {totalApiResultsPages > 1 &&
-//             currentApiResultPage !== totalApiResultsPages && (
-//               <button
-//                 onClick={handleClick}
-//                 className="text-black bg-white hover:bg-gray-200 transition"
-//               >
-//                 تحميل المزيد
-//               </button>
-//             )}
+//         <button
+//           onClick={handleClose}
+//           className="mb-2 mr-2 rounded-md bg-white px-2 py-1 transition hover:bg-gray-200"
+//         >
+//           إغلاق
+//         </button>
+//         <ul className="flex flex-col gap-3">
+//           {results &&
+//             results.map((el, index) => {
+//               return (
+//                 <li key={index}>
+//                   <Link
+//                     to={`quran/page/${el.pageNumber}`}
+//                     onClick={handleClose}
+//                     className="flex flex-wrap gap-2 bg-gray-700 px-1 py-2 text-lg leading-[40px] text-white transition hover:bg-gray-800"
+//                   >
+//                     <p>{el.ayah}</p>
+//                     <span className="bg-black px-1">صفحة {el.pageNumber}</span>
+//                   </Link>
+//                 </li>
+//               );
+//             })}
+//         </ul>
+//         <div className={`flex gap-2 ${resultApiPages <= 1 && "hidden"}`}>
+//           <button
+//             onClick={() => {
+//               if (currentApiResultPage > 1) {
+//                 setCurrentApiResultPage((prev) => prev - 1);
+//               }
+//             }}
+//             className={`mt-2 w-full rounded-md bg-white py-1 hover:bg-gray-200 ${
+//               currentApiResultPage === 1 && "hidden"
+//             }`}
+//           >
+//             السابق
+//           </button>
+//           <button
+//             onClick={() => {
+//               if (currentApiResultPage < resultApiPages) {
+//                 setCurrentApiResultPage((prev) => prev + 1);
+//               }
+//             }}
+//             className={`mt-2 w-full rounded-md bg-white py-1 hover:bg-gray-200 ${
+//               currentApiResultPage === resultApiPages && "hidden"
+//             }`}
+//           >
+//             التالي
+//           </button>
 //         </div>
 //       </div>
 //     </>
--- src/components/Surah.jsx
+++ src/components/Surah.jsx
@@ -1,92 +1,98 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+// //
+// //
+// // future Idea
+// //
+// //
 
-export default function Surah() {
-  let { surah } = useParams();
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
-  const [surahs, setSurahs] = useState([]);
-  const [arabicSurahName, setArabicSurahName] = useState("");
-  const [surahNumber, setSurahNumber] = useState();
-  const [ayahs, setAyahs] = useState([]);
+// export default function Surah() {
+//   let { surah } = useParams();
 
-  useEffect(() => {
-    fetch(`//api.alquran.cloud/v1/surah`)
-      .then((res) => res.json())
-      .then((data) => setSurahs(data.data));
-  }, []);
+//   const [surahs, setSurahs] = useState([]);
+//   const [arabicSurahName, setArabicSurahName] = useState("");
+//   const [surahNumber, setSurahNumber] = useState();
+//   const [ayahs, setAyahs] = useState([]);
 
-  useEffect(() => {
-    surahs.map((surahName) => {
-      if (surahName.englishName === surah) {
-        setArabicSurahName(surahName.name);
-        setSurahNumber(surahName.number);
-      }
-    });
+//   useEffect(() => {
+//     fetch(`//api.alquran.cloud/v1/surah`)
+//       .then((res) => res.json())
+//       .then((data) => setSurahs(data.data));
+//   }, []);
 
-    if (surahNumber) {
-      setAyahs([]);
-      fetch(`//api.alquran.cloud/v1/surah/${surahNumber}`)
-        .then((res) => res.json())
-        .then((data) => {
-          data.data.ayahs ? setAyahs([...data.data.ayahs]) : false;
-        });
-    }
-  }, [surahs, surahNumber, surah]);
+//   useEffect(() => {
+//     surahs.map((surahName) => {
+//       if (surahName.englishName === surah) {
+//         setArabicSurahName(surahName.name);
+//         setSurahNumber(surahName.number);
+//       }
+//     });
 
-  return (
-    <>
-      {/* <audio
-        controls
-        className="w-full container fixed left-1/2 -translate-x-1/2 bottom-0"
-      >
-        <source
-          src={`https://cdn.islamic.network/quran/audio-surah/128/ar.alafasy/${surahNumber}.mp3`}
-          type="audio/mp3"
-        />
-      </audio> */}
+//     if (surahNumber) {
+//       setAyahs([]);
+//       fetch(`//api.alquran.cloud/v1/surah/${surahNumber}`)
+//         .then((res) => res.json())
+//         .then((data) => {
+//           data.data.ayahs ? setAyahs([...data.data.ayahs]) : false;
+//         });
+//     }
+//   }, [surahs, surahNumber, surah]);
 
-      <div className="px-3 py-3 text-primary-white md:px-10">
-        <div className="flex items-center justify-around gap-3 pt-2">
-          <div className="text-center text-sm">رقم السورة: {surahNumber}</div>
-          <h1 className="text-center text-4xl">{arabicSurahName}</h1>
-          <span className="text-sm">عدد الآيات: {ayahs.length}</span>
-        </div>
-        <div className="flex flex-wrap gap-2 pt-6">
-          {ayahs.length ? (
-            <p className="text-2xl leading-[60px]">
-              {ayahs.map((ayah) => {
-                if (
-                  ayah.text.includes(
-                    `بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ`,
-                  ) &&
-                  ayah.numberInSurah === 1
-                ) {
-                  return (
-                    <>
-                      <i className="block">
-                        بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ
-                      </i>
-                      {`${
-                        ayah.text.split(
-                          `بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ`,
-                        )[1]
-                      } ${`\u06DD${ayah.numberInSurah}`} `}
-                    </>
-                  );
-                }
-                return `${ayah.text} ${`\u06DD${ayah.numberInSurah}`} `;
-              })}
-            </p>
-          ) : (
-            <FontAwesomeIcon
-              icon={faCircleNotch}
-              className="mt-20 w-full animate-spin text-center text-4xl"
-            />
-          )}
-        </div>
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       {/* <audio
+//         controls
+//         className="w-full container fixed left-1/2 -translate-x-1/2 bottom-0"
+//       >
+//         <source
+//           src={`https://cdn.islamic.network/quran/audio-surah/128/ar.alafasy/${surahNumber}.mp3`}
+//           type="audio/mp3"
+//         />
+//       </audio> */}
+
+//       <div className="px-3 py-3 text-primary-white md:px-10">
+//         <div className="flex items-center justify-around gap-3 pt-2">
+//           <div className="text-center text-sm">رقم السورة: {surahNumber}</div>
+//           <h1 className="text-center text-4xl">{arabicSurahName}</h1>
+//           <span className="text-sm">عدد الآيات: {ayahs.length}</span>
+//         </div>
+//         <div className="flex flex-wrap gap-2 pt-6">
+//           {ayahs.length ? (
+//             <p className="text-2xl leading-[60px]">
+//               {ayahs.map((ayah) => {
+//                 if (
+//                   ayah.text.includes(
+//                     `بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ`,
+//                   ) &&
+//                   ayah.numberInSurah === 1
+//                 ) {
+//                   return (
+//                     <>
+//                       <i className="block">
+//                         بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ
+//                       </i>
+//                       {`${
+//                         ayah.text.split(
+//                           `بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ`,
+//                         )[1]
+//                       } ${`\u06DD${ayah.numberInSurah}`} `}
+//                     </>
+//                   );
+//                 }
+//                 return `${ayah.text} ${`\u06DD${ayah.numberInSurah}`} `;
+//               })}
+//             </p>
+//           ) : (
+//             <FontAwesomeIcon
+//               icon={faCircleNotch}
+//               className="mt-20 w-full animate-spin text-center text-4xl"
+//             />
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
