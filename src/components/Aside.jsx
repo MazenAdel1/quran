72f0e93ce9ba137fda83bae5da7700d248329559
Mazen Adel <ma134652134652@gmail.com>
2024-01-13 00:13:30 +0200

    refactor: use navbar component

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,33 +1,13 @@
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRef, useState } from "react";
-import ListOfSurahs from "./ListOfSurahs";
-import ListOfJuzs from "./ListOfJuzs";
-import ListOfBookMarks from "./ListOfBookMarks";
+import { useRef } from "react";
+import Navbar from "./Navbar";
 
 export default function Aside() {
   const asideRef = useRef();
 
-  const [categories, setCategories] = useState([
-    { title: "السور", active: true },
-    { title: "الأجزاء", active: false },
-    { title: "المرجعيات", active: false },
-  ]);
-
   const handlingAsideStatus = () => {
     asideRef.current.classList.toggle(`right-0`);
-  };
-
-  const handlingCategoryClick = (e) => {
-    setCategories((currCategory) => {
-      return currCategory.map((category) => {
-        if (category.title == e.target.textContent) {
-          return { ...category, active: true };
-        } else {
-          return { ...category, active: false };
-        }
-      });
-    });
   };
 
   return (
@@ -40,29 +20,9 @@ export default function Aside() {
       </button>
       <aside
         ref={asideRef}
-        className="fixed -right-full top-14 z-10 flex h-[calc(100dvh-44px)] w-[250px] flex-col overflow-y-auto bg-[#101a2e] text-3xl text-white transition-all duration-300 sm:w-[400px]"
+        className="fixed -right-full top-14 z-10 flex h-[calc(100dvh-44px)] w-[250px] flex-col overflow-y-auto bg-[#101a2e] text-3xl transition-all duration-300 sm:w-[400px]"
       >
-        <nav className="relative z-10">
-          <ul className="sticky right-0 top-0 z-50 flex w-full bg-orange">
-            {categories.map((el) => {
-              return (
-                <li className="flex-1 text-xl sm:text-3xl" key={el.title}>
-                  <button
-                    className={`w-full py-1 text-center transition hover:bg-[#ffb066] ${
-                      el.active && "bg-[#d8873b]"
-                    }`}
-                    onClick={handlingCategoryClick}
-                  >
-                    {el.title}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-          {categories[0].active && <ListOfSurahs />}
-          {categories[1].active && <ListOfJuzs />}
-          {categories[2].active && <ListOfBookMarks />}
-        </nav>
+        <Navbar aside />
       </aside>
     </>
   );
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,31 +1,11 @@
-import { useState } from "react";
-import ListOfSurahs from "./ListOfSurahs";
-import ListOfJuzs from "./ListOfJuzs";
-import ListOfBookMarks from "./ListOfBookMarks";
+import Navbar from "./Navbar";
 
 export default function LandingPage() {
-  const [categories, setCategories] = useState([
-    { title: "السور", active: true },
-    { title: "الأجزاء", active: false },
-    { title: "المرجعيات", active: false },
-  ]);
-
-  const handlingCategoryClick = (e) => {
-    setCategories((currCategory) => {
-      return currCategory.map((category) => {
-        if (category.title == e.target.textContent) {
-          return { ...category, active: true };
-        } else {
-          return { ...category, active: false };
-        }
-      });
-    });
-  };
-
   return (
     <>
       <div className="absolute -z-10 h-[100px] w-[100px] rounded-full bg-secondary blur-[200px]"></div>
       <div className="absolute left-0 top-1/2 -z-10 h-[100px] w-[100px] -translate-y-1/2 rounded-full bg-secondary blur-[200px]"></div>
+
       <div className="mb-10 flex flex-col items-center gap-5 px-5 pt-8 text-primary-white">
         <h1 className="text-2xl font-bold underline">في فضل القرآن</h1>
         <p className="text-center text-xl">
@@ -38,30 +18,8 @@ export default function LandingPage() {
         </p>
       </div>
 
-      <div className="container mx-auto sm:px-32 ">
-        <nav className="container relative z-10 mx-auto w-full">
-          <div className="sticky h-full sm:absolute sm:left-full">
-            <ul className="sticky right-0 top-0 z-50 flex w-full bg-orange sm:right-0 sm:top-0 sm:w-fit sm:flex-col">
-              {categories.map((el) => {
-                return (
-                  <li className="flex-1 text-xl sm:text-3xl" key={el.title}>
-                    <button
-                      className={`w-full py-3 text-center transition hover:bg-[#ffb066] sm:px-2 ${
-                        el.active && "bg-[#d8873b]"
-                      }`}
-                      onClick={handlingCategoryClick}
-                    >
-                      {el.title}
-                    </button>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div>{categories[0].active && <ListOfSurahs />}</div>
-          <div>{categories[1].active && <ListOfJuzs />}</div>
-          <div>{categories[2].active && <ListOfBookMarks />}</div>
-        </nav>
+      <div className="container mx-auto pt-[44px] sm:px-32">
+        <Navbar landingPage />
       </div>
     </>
   );
